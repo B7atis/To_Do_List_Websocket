646a6f90c34a8e8f373b71a6a3ad593a6a6f71b4
Bartosz Jędrzejewski-Emche <jedrek2603@gmail.com>
2021-08-13 16:21:00 +0200

    Add functionality to server.

--- server.js
+++ server.js
@@ -1,10 +1,31 @@
 const express = require('express');
+const socket = require('socket.io');
 
 const app = express();
 
+const tasks = [];
+
 const server = app.listen(process.env.PORT || 8000, () => {
-  console.log('Server is running...');
+  console.log('Server is running on Port:', 8000);
 });
+const io = socket(server);
+
+io.on('connection', (socket) => {
+  console.log("New client! It's id - " + socket.id);
+
+  io.to(socket.id).emit('updateData', tasks);
+  socket.on('addTask', (task) => {
+    tasks.push(task);
+    socket.broadcast.emit('addTask', task);
+  });
+
+  socket.on('removeTask', (taskId) => {
+    const task = tasks.find((task) => task.id === taskId);
+    const index = tasks.indexOf(task);
+    tasks.splice(index, 1);
+    socket.broadcast.emit('removeTask', taskId);
+  });
+})
 
 app.use((req, res) => {
   res.status(404).send({ message: 'Not found...' });
